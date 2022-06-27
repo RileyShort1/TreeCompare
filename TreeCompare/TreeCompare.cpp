817f0fb7938c4ecf1a337dd57a974e6a2525cd95
Riley Short <rileyshort1@gmail.com>
2022-06-26 19:02:46 -0700

    added link for references on trees

--- TreeCompare/TreeCompare.cpp
+++ TreeCompare/TreeCompare.cpp
@@ -4,6 +4,10 @@
 #include "AVL_Tree.h"
 #include <iostream>
 
+//
+// Link to a data structure visualization tool (https://www.cs.usfca.edu/~galles/visualization/Algorithms.html)
+//
+
 int main()
 {
     std::cout << "Hello World!\n";
