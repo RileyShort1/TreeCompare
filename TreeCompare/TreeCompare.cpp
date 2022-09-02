16c3a83c48f329c81f6541dd59a6f6f466527f4b
Riley Short <rileyshort1@gmail.com>
2022-09-02 13:36:41 -0700

    Added File output

--- TreeCompare/TreeCompare.cpp
+++ TreeCompare/TreeCompare.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <fstream>
 
 #include "SplayTree.h"
 #include "AVL_Tree.h"
@@ -16,11 +17,21 @@ int main(){
     const size_t num_trees_per_run = 100;
     const size_t num_inserts_per_tree = 100 * 1000;
     const size_t num_finds_per_tree = 500 * 1000;
+    int programTime = 0;
 
+    std::fstream result_file; // output file
+    result_file.open("TestResults.csv", std::ios::out | std::ios::app);
+    result_file << "Max stddev = " << max_std_dev_tested << ", " << "Mean: " << expt_mean << ", " <<
+        "Num Trees per run: " << num_trees_per_run << ", " << "Num Inserts per tree: " << num_inserts_per_tree <<
+        ", " << "Num finds per tree: " << num_finds_per_tree << ", " << "Num random runs: " << num_random_runs << "\n"
+        << "Sigma" << ", " << "Splay Time" << ", " << "AVL Time" << ", " << "Tree Size\n";
+    
     for (int n = 0; n < num_random_runs; n++) {
+        programTime++;
+        std::cout << programTime << " / " << num_random_runs << "\n";
         for (int sigma = 0; sigma < max_std_dev_tested; sigma += std_dev_step) {
             std::mt19937 randEngine(n); //seed
-            std::normal_distribution<double> generator(expt_mean, sigma);
+            std::normal_distribution<double> generator(expt_mean, double(sigma));
             
             for (int t = 0; t < num_trees_per_run; t++) {
                 
@@ -36,7 +47,7 @@ int main(){
                 }
                 
                 //At this point, both our trees are ready to test and filled with identical randoms
-                cout << avl_tree.get_size() << "\t";
+                //cout << "Tree size: " << avl_tree.get_size() << "\t";
                 
                 //time our find methods
                 double total_avl_find_time = 0, total_splay_find_time = 0;
@@ -57,10 +68,13 @@ int main(){
                     total_splay_find_time += diff.count();
                     
                 }
-                cout << sigma << "\t" << total_splay_find_time << "\t" << total_avl_find_time << endl;
+                //cout << "Sigma: " << sigma + 1 << "\t" << total_splay_find_time << "\t" << total_avl_find_time << endl;
+                result_file << sigma << total_splay_find_time << total_avl_find_time << avl_tree.get_size() << "\n";
             } //num trees
         } //sigma
     } //seeds
+    
+    result_file.close();
     
     return 0;
 }
