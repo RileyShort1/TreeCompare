4be34358805f8217d09c36a71d4b985ea1bf0606
walter-bergstrom <walter.j.bergstrom@gmail.com>
2022-09-14 23:12:50 -0700

    New main (unfinished)
    This is the new main created for the tests. Header files still need to be updated.

--- TreeCompare/TreeCompare.cpp
+++ TreeCompare/TreeCompare.cpp
@@ -3,13 +3,15 @@
 #include <chrono>
 #include <fstream>
 
-#include "SplayTree.h"
+//#include "BST.h"
+//#include "Tree_Algorithms.h"
 #include "AVL_Tree.h"
+#include "SplayTree.h"
 
 using namespace std;
 
 
-int main(){
+int main() {
     const size_t max_std_dev_tested = 100 * 1000;
     const size_t expt_mean = 50 * 1000; //expect nums to be centered around here, sigma determines spread
     const size_t std_dev_step = 50;
@@ -17,36 +19,36 @@ int main(){
     const size_t num_trees_per_run = 100;
     const size_t num_inserts_per_tree = 100 * 1000;
     const size_t num_finds_per_tree = 500 * 1000;
-    int programTime = 0;
-
-    std::fstream result_file; // output file
-    result_file.open("TestResults.csv", std::ios::out | std::ios::app);
-    result_file << "Max stddev = " << max_std_dev_tested << ", " << "Mean: " << expt_mean << ", " <<
-        "Num Trees per run: " << num_trees_per_run << ", " << "Num Inserts per tree: " << num_inserts_per_tree <<
-        ", " << "Num finds per tree: " << num_finds_per_tree << ", " << "Num random runs: " << num_random_runs << "\n"
-        << "Sigma" << ", " << "Splay Time" << ", " << "AVL Time" << ", " << "Tree Size\n";
     
     for (int n = 0; n < num_random_runs; n++) {
-        programTime++;
-        std::cout << programTime << " / " << num_random_runs << "\n";
+        cout << endl;
+        cout << "#SEED = " << n << ":\n";
+        cout << "###############################################################";
+        
+        //sigma can't be 0 because the generator does not accept it
         for (int sigma = 1; sigma <= max_std_dev_tested; sigma += std_dev_step) {
             std::mt19937 randEngine(n); //seed
             std::normal_distribution<double> generator(expt_mean, (double) sigma);
             
+            cout << "\n#SIGMA, SPLAY TIME, AVL TIME, TREE SIZE\n";  //FIXED
+            vector<double> avg_over_all_trees{0,0,0,0};
             for (int t = 0; t < num_trees_per_run; t++) {
                 
                 AVL_Tree<int> avl_tree;
-                SplayTree<int> splay_tree;
+                //SplayTree<int> splay_tree;
+                BST<int> splay_tree;
+                
+                
                             
                 //Make our full trees
                 for (size_t i = 0; i < num_inserts_per_tree; i++) {
                     int my_random = (int)generator(randEngine);
                     avl_tree.insert(my_random);
-                    splay_tree.insert(my_random);
+                    //splay_tree.insert(my_random);
+                    Tx::splay_insert(splay_tree, my_random);
                 }
                 
                 //At this point, both our trees are ready to test and filled with identical randoms
-                //cout << "Tree size: " << avl_tree.get_size() << "\t";
                 
                 //time our find methods
                 double total_avl_find_time = 0, total_splay_find_time = 0;
@@ -61,19 +63,30 @@ int main(){
                     
                     //same thing for splay
                     then = std::chrono::high_resolution_clock::now();
-                    (void) splay_tree.contains(num_to_find);
+                    //(void) splay_tree.contains(num_to_find);
+                    Tx::splay_contains(splay_tree, num_to_find);
                     now = std::chrono::high_resolution_clock::now();
                     diff = now - then;
                     total_splay_find_time += diff.count();
                     
                 }
-                //cout << "Sigma: " << sigma + 1 << "\t" << total_splay_find_time << "\t" << total_avl_find_time << endl;
-                result_file << sigma << ", " << total_splay_find_time << ", " << total_avl_find_time << ", " << avl_tree.get_size() << "\n";
+                cout << sigma << ", " << total_splay_find_time << ", " << total_avl_find_time << ", " << splay_tree.get_size() << endl;             //changed to splay
+                avg_over_all_trees[0] += sigma;
+                avg_over_all_trees[1] += total_splay_find_time;
+                avg_over_all_trees[2] += total_avl_find_time;
+                avg_over_all_trees[3] += splay_tree.get_size();   //changed to splay
+                
+                avl_tree.clear();     //Fix AVL deconstructor and remove this line afterwards
+                
             } //num trees
+            cout << "#Avg: " << avg_over_all_trees[0] / num_trees_per_run
+                 << ", "   << avg_over_all_trees[1] / num_trees_per_run
+                 << ", "   << avg_over_all_trees[2] / num_trees_per_run
+                 << ", "   << avg_over_all_trees[3] / num_trees_per_run;
+            //return 0;   //temporary
         } //sigma
+        //return 0;      //temporary
     } //seeds
-    
-    result_file.close();
     
     return 0;
 }
